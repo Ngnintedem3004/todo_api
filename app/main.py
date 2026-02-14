df72369da73d982da18c09bcae39f6c0237ed696
Ngnintedem3004 <ngnintedemmelong@gmail.com>
2026-02-14 15:08:00 +0100

    Modified Main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers.tasks_db import router as tasks_router
-from database import Base ,engine
+from database import Base 
 
 app = FastAPI(title="Todo API", description="A simple Todo API built with FastAPI", version="1.1.0")
 app.include_router(tasks_router) 
